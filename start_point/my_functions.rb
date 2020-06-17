daf240be7f2ea40691e23a8efc5fc2d1c6b74507
Rebeka Geczi <geczirebeka@gmail.com>
2020-06-17 14:44:15 +0100

    second commit

---
+++ start_point/my_functions.rb
@@ -0,0 +1,12 @@
+def add_array_lengths(array_1, array_2)
+    return array_1.length + array_2.length
+end
+
+
+def sum_array(numbers)
+    total = 0
+    for number in numbers
+        total += number
+    end
+    return total
+end
